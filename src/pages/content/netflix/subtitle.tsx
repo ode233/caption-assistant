e4e2e83909d792002f787d17ed88a4d8f2dbbbc0
ode233 <ody122333@gmail.com>
2022-08-17 02:23:55 +0800

    process line break

--- src/pages/content/netflix/subtitle.tsx
+++ src/pages/content/netflix/subtitle.tsx
@@ -17,10 +17,33 @@ function getSubtitleNodeList(e: any) {
         const begin = Number(beginString) / 10 ** 7;
         const endString = subtitleElement.getAttribute('end')?.replace('t', '');
         const end = Number(endString) / 10 ** 7;
-        subtitleElement.setAttribute('index', i.toString());
+        subtitleElement = getNewSubtitleElement(subtitleElement, i);
         let subtitle = new SubtitleNode(begin, end, subtitleElement);
         subtitleNodeList.push(subtitle);
     }
+    console.log(subtitleNodeList);
+}
+
+function getNewSubtitleElement(
+    subtitleElement: HTMLParagraphElement,
+    index: number
+): HTMLParagraphElement {
+    let newSubtitleElement = document.createElement('p');
+    newSubtitleElement.setAttribute('index', index.toString());
+
+    let newSpan = document.createElement('span');
+    newSpan.style.whiteSpace = 'pre-line';
+    let text = '';
+    let spanList = subtitleElement.getElementsByTagName('span');
+    for (let i = 0; i < spanList.length - 1; i++) {
+        let span = spanList[i];
+        text += span.textContent + '&#10;';
+    }
+    text += spanList[spanList.length - 1].textContent;
+    newSpan.innerHTML = text;
+    newSubtitleElement.appendChild(newSpan);
+
+    return newSubtitleElement;
 }
 
 export { getSubtitleNodeList, subtitleNodeList };
