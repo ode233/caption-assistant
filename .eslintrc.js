c0b2af40aff0c461345ed19bda265b2d72eca397
ode233 <ody122333@gmail.com>
2022-08-31 03:04:40 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,8 @@ module.exports = {
                     'globals',
                     'tsx',
                     'youdao',
-                    'tgt'
+                    'tgt',
+                    'anki'
                 ]
             }
         ]
--- src/pages/content/translate/popup.tsx
+++ src/pages/content/translate/popup.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { getText, getSentence } from 'get-selection-more';
-import { BsVolumeUpFill } from 'react-icons/bs';
+import { BsVolumeUpFill, BsXLg } from 'react-icons/bs';
+import { BiExport } from 'react-icons/bi';
 
-const popupId = 'popup-id-866699#$%114';
-const popupWidth = 400;
-const popupHeight = 300;
+const dictPopupWidth = 400;
+const dictPopupHeight = 300;
+
+const ankiPopupWidth = 500;
+const ankiPopupHeight = 800;
 
 // eslint-disable-next-line spellcheck/spell-checker
 const DictPopupWrapper = styled.div<{ display: string; left: number; top: number }>`
@@ -16,16 +19,40 @@ const DictPopupWrapper = styled.div<{ display: string; left: number; top: number
     margin: auto;
     padding: 20px;
     border: 1px solid #888;
-    width: ${popupWidth + 'px'};
-    height: ${popupHeight + 'px'};
+    width: ${dictPopupWidth + 'px'};
+    height: ${dictPopupHeight + 'px'};
     z-index: 10001;
     display: ${(props) => props.display};
     left: ${(props) => props.left + 'px'};
     top: ${(props) => props.top + 'px'};
 `;
 
+// eslint-disable-next-line spellcheck/spell-checker
+const AnkiPopupWrapper = styled.div<{ display: string }>`
+    overflow: auto;
+    position: absolute;
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: ${ankiPopupWidth + 'px'};
+    height: ${ankiPopupHeight + 'px'};
+    z-index: 10001;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    display: ${(props) => props.display};
+`;
+
+const Text = styled.h3`
+    font-size: large;
+    font-weight: normal;
+    font-family: sans-serif;
+`;
+
 const Popup = () => {
-    const [display, setDisplay] = useState<string>('none');
+    const [dictDisplay, setDictDisplay] = useState<string>('none');
     const [left, setLeft] = useState<number>(0);
     const [top, setTop] = useState<number>(0);
 
@@ -37,6 +64,8 @@ const Popup = () => {
     const [sentenceTranslate, setSentenceTranslate] = useState<string>('');
     const [sentenceVoiceUrl, setSentenceVoiceUrl] = useState<string>('');
 
+    const [ankiDisplay, setAnkiDisplay] = useState<string>('none');
+
     const leftRef = useRef(left);
     const topRef = useRef(top);
 
@@ -46,7 +75,6 @@ const Popup = () => {
     });
 
     useEffect(() => {
-        let targetElement: EventTarget | null;
         document.addEventListener('mouseup', (event: MouseEvent) => {
             let text = getText();
             if (!text) {
@@ -78,18 +106,18 @@ const Popup = () => {
             setTextVoiceUrl(`https://dict.youdao.com/dictvoice?type=0&audio=${text}`);
             setSentence(sentence);
             setSentenceVoiceUrl(`https://dict.youdao.com/dictvoice?type=0&audio=${sentence}`);
-            setLeft(event.pageX);
-            setTop(event.pageY);
-            setDisplay('block');
+            setLeft(event.pageX + 10);
+            setTop(event.pageY + 10);
+            setDictDisplay('block');
         });
 
         document.addEventListener('mousedown', (event: MouseEvent) => {
-            setDisplay((display) => {
+            setDictDisplay((display) => {
                 if (
                     (event.pageX < leftRef.current ||
-                        event.pageX > leftRef.current + popupWidth ||
+                        event.pageX > leftRef.current + dictPopupWidth ||
                         event.pageY < topRef.current ||
-                        event.pageY > topRef.current + popupHeight) &&
+                        event.pageY > topRef.current + dictPopupHeight) &&
                     display === 'block'
                 ) {
                     window.getSelection()?.removeAllRanges();
@@ -100,34 +128,61 @@ const Popup = () => {
         });
     }, []);
 
+    let onClickExportToAnki = () => {
+        setDictDisplay('none');
+        setAnkiDisplay('block');
+    };
+
     return (
-        <DictPopupWrapper id={popupId} display={display} left={left} top={top}>
-            <h3>
-                {text}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                {phonetic}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <BsVolumeUpFill
+        <div>
+            <DictPopupWrapper display={dictDisplay} left={left} top={top}>
+                <Text>
+                    {text}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    {phonetic}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <BsVolumeUpFill
+                        style={{ fontSize: 'larger', verticalAlign: 'text-bottom' }}
+                        onClick={() => {
+                            let audio = new Audio(textVoiceUrl);
+                            audio.play();
+                        }}
+                    />
+                    <BiExport
+                        style={{
+                            right: 0,
+                            marginRight: '30px',
+                            position: 'absolute',
+                            fontSize: 'larger',
+                            verticalAlign: 'text-bottom'
+                        }}
+                        onClick={onClickExportToAnki}
+                    />
+                </Text>
+                <Text style={{ marginTop: '15px' }}>{textTranslate}</Text>
+                <hr style={{ marginTop: '20px', marginBottom: '20px', borderBottom: '1px solid black' }} />
+                <Text>
+                    {sentence}
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <BsVolumeUpFill
+                        style={{ fontSize: 'larger', verticalAlign: 'text-bottom' }}
+                        onClick={() => {
+                            let audio = new Audio(sentenceVoiceUrl);
+                            audio.play();
+                        }}
+                    />
+                </Text>
+                <Text style={{ marginTop: '15px' }}>{sentenceTranslate}</Text>
+            </DictPopupWrapper>
+            <AnkiPopupWrapper display={ankiDisplay}>
+                <BsXLg
+                    style={{ right: 0, marginRight: '20px', position: 'absolute', fontSize: 'larger' }}
                     onClick={() => {
-                        let audio = new Audio(textVoiceUrl);
-                        audio.play();
+                        setAnkiDisplay('none');
                     }}
                 />
-            </h3>
-            <h3 style={{ marginTop: '10px' }}>{textTranslate}</h3>
-            <hr style={{ marginTop: '20px', marginBottom: '20px' }} />
-            <h3>
-                {sentence}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <BsVolumeUpFill
-                    onClick={() => {
-                        let audio = new Audio(sentenceVoiceUrl);
-                        audio.play();
-                    }}
-                />
-            </h3>
-            <h3 style={{ marginTop: '10px' }}>{sentenceTranslate}</h3>
-        </DictPopupWrapper>
+            </AnkiPopupWrapper>
+        </div>
     );
 };
 
