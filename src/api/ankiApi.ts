cec8f0838b9b580fada81cc1a57acf209ba80f93
ode233 <ody122333@gmail.com>
2023-01-08 11:45:28 +0800

    update anki card

--- src/api/ankiApi.ts
+++ src/api/ankiApi.ts
@@ -1,5 +1,11 @@
 import { PopupProps } from '../components/content/translate/popup';
-import { ANKI_DECK_NAME, ANKI_MODEL_NAME } from '../constants/ankiConstants';
+import {
+    ANKI_CARD_BACK_HTML,
+    ANKI_CARD_CSS,
+    ANKI_CARD_FRONT_HTML,
+    ANKI_DECK_NAME,
+    ANKI_MODEL_NAME
+} from '../constants/ankiConstants';
 
 const ankiBaseUrl = 'http://localhost:8765';
 
@@ -48,137 +54,6 @@ export const getModelNames = async () => {
     return response.json();
 };
 
-const frontHTML = `
-<section>{{cloze:SentenceCloze}}<section>
-
-<section>{{type:cloze:SentenceCloze}}</section>
-
-<section>{{TextTranslate}}</section>
-
-<section>{{SentenceTranslate}}</section>
-
-<section>TextVoice: {{TextVoice}} {{TextPhonetic}}</section>
-
-<section>SentenceVoice: {{SentenceVoice}}</section>
-
-{{#Remark}}
-    <section>{{Remark}}<section>
-{{/Remark}}
-
-<section class="source">
-<hr />
-{{PageIcon}}
-<a href="{{PageUrl}}">{{PageTitle}}</a>
-</section>
-
-<section class="srcImg">
-{{Img}}
-<section>
-`;
-
-const backHTML = `
-<section>{{cloze:SentenceCloze}}<section>
-
-<section>{{type:cloze:SentenceCloze}}</section>
-
-<section>{{TextTranslate}}</section>
-
-<section>{{SentenceTranslate}}</section>
-
-<section>TextVoice: {{TextVoice}} {{TextPhonetic}}</section>
-
-<section>SentenceVoice: {{SentenceVoice}}</section>
-
-{{#Remark}}
-    <section>{{Remark}}<section>
-{{/Remark}}
-
-<section class="source">
-<hr />
-{{PageIcon}}
-<a href="{{PageUrl}}">{{PageTitle}}</a>
-</section>
-
-<section class="srcImg">
-{{Img}}
-<section>
-`;
-
-// eslint-disable-next-line spellcheck/spell-checker
-const css = `
-.card {
-    font-family: arial;
-    font-size: 20px;
-    text-align: center;
-    color: #333;
-    background-color: white;
-  }
-  
-  a {
-    color: #5caf9e;
-  }
-  
-  input {
-    border: 1px solid #eee;
-  }
-  
-  section {
-    margin: 1em 0;
-  }
-  
-  .trans {
-    border: 1px solid #eee;
-    padding: 0.5em;
-  }
-  
-  .trans_title {
-    display: block;
-    font-size: 0.9em;
-    font-weight: bold;
-  }
-  
-  .trans_content {
-    margin-bottom: 0.5em;
-  }
-  
-  .cloze {
-    font-weight: bold;
-    color: #f9690e;
-  }
-  
-  .source {
-    position: relative;
-    font-size: .8em;
-  }
-  
-  .source img {
-    height: .7em;
-  }
-  
-  .source a {
-    text-decoration: none;
-  }
-  
-  .typeGood {
-    color: #fff;
-    background: #1EBC61;
-  }
-  
-  .typeBad {
-    color: #fff;
-    background: #F75C4C;
-  }
-  
-  .typeMissed {
-    color: #fff;
-    background: #7C8A99;
-  }
-
-  .source img {
-    width: inherit;
-  }
-`;
-
 export const createModel = async () => {
     const response = await fetch(ankiBaseUrl, {
         method: 'POST',
@@ -207,12 +82,12 @@ export const createModel = async () => {
                     'SentenceCloze'
                 ],
                 isCloze: true,
-                css: css,
+                css: ANKI_CARD_CSS,
                 cardTemplates: [
                     {
                         Name: 'Card',
-                        Front: frontHTML,
-                        Back: backHTML
+                        Front: ANKI_CARD_FRONT_HTML,
+                        Back: ANKI_CARD_BACK_HTML
                     }
                 ]
             }
